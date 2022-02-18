deaa403dfe7552afc28b19e005768546c8159581
s0je <cuentasegurajss@gmail.com>
2022-02-18 10:56:01 +0100

    miniproyecto Update

--- miniProyecto/src/routes/adicionales.js
+++ miniProyecto/src/routes/adicionales.js
@@ -35,3 +35,57 @@ router.get("/",
                 })
         }
 )
+
+router.get("/",
+        function(request,response)
+        {
+            let id = request.query.id
+            let params = [id]
+            let sql;
+            if(request.query.id == null)
+                sql = `SELECT sb.title, s.first_name, s.last_nameFROM subjects AS sb 
+                        JOIN subjects_teacher AS st ON (sb.subject_id = st.subject_id)
+                        JOIN students AS s ON (st.group_id = s.group_id)`
+            else
+            let sql = `SELECT sb.title FROM subjects AS sb 
+                        JOIN subjects_teacher AS st ON (sb.subject_id = st.subject_id)
+                        JOIN students AS s ON (st.group_id = s.group_id)
+                        WHERE s.student_id = ?`;
+            connection.query(sql,[params], function (err, result) 
+            {
+                if (err) 
+                    console.log(err);
+                else 
+                {
+                    response.send(result);
+                }
+            })
+        }
+)
+
+router.get("/",
+        function(request,response)
+        {
+            let id = request.query.id
+            let params = [id]
+            let sql;
+            if(request.query.id == null)
+                sql = `SELECT sb.title, t.first_name, t.last_nameFROM subjects AS sb 
+                        JOIN subjects_teacher AS st ON (sb.subject_id = st.subject_id)
+                        JOIN teachers AS t ON (st.teacher_id = t.teacher_id)`
+            else
+            let sql = `SELECT sb.title FROM subjects AS sb 
+                        JOIN subjects_teacher AS st ON (sb.subject_id = st.subject_id)
+                        JOIN teachers AS t ON (st.teacher_id = t.teacher_id)
+                        WHERE t.teacher_id = ?`;
+            connection.query(sql,[params], function (err, result) 
+            {
+                if (err) 
+                    console.log(err);
+                else 
+                {
+                    response.send(result);
+                }
+            })
+        }
+)
